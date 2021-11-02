a9de3ce26958747a5d555282316d0119570785cb
iserra <i.serra@.creaf.uab.cat>
2021-11-02 12:36:54 +0100

    changes folder location

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-unzip data/s2.zip -d /tmp/unzipped
-
-python ex1.py /tmp/unzipped/*.SAFE
+#unzip data/s2.zip -d /tmp/unzipped
+unzip data/s2.zip
+python ex1.py *.SAFE
+#python ex1.py /tmp/unzipped/*.SAFE
+#$(ls -d /tmp/unzipped/*.SAFE)
