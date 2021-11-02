bf19c610ad3da98b4220e5ed0a898ab3931d570e
iserra <i.serra@.creaf.uab.cat>
2021-11-02 13:33:11 +0100

    changes run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #unzip data/s2.zip -d /tmp/unzipped
-unzip data/s2.zip
-python ex1.py *.SAFE
+unzip data/sat_product.zip -d /tmp/sat_product
+python ex1.py /tmp/sat_product/*.SAFE
 #python ex1.py /tmp/unzipped/*.SAFE
 #$(ls -d /tmp/unzipped/*.SAFE)
