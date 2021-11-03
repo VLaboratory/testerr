f8789d39e87f62475673a6e7a6bc1aa6b5b396bb
iserra <i.serra@.creaf.uab.cat>
2021-11-03 18:13:31 +0100

    minor changes

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #unzip data/s2.zip -d /tmp/unzipped
-unzip data/sat_product.zip -d /tmp/sat_product
+unzip data/sat_product.zip -d tmp/sat_product
 #python ex1.py /tmp/sat_product/*.SAFE
 #python ex1.py $(ls -d /tmp/sat_product/*.SAFE)
 
-python ex1_01.py ./$(ls -d /tmp/sat_product/*.SAFE)
+python ex1_01.py /$(ls -d tmp/sat_product/*.SAFE)
 
 #python ex1.py /tmp/unzipped/*.SAFE
 #$(ls -d /tmp/unzipped/*.SAFE)
