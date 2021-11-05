a617975c573b8adba4b2eac409dd659295ebc4c1
iserra <i.serra@.creaf.uab.cat>
2021-11-05 12:51:15 +0100

    changes debug lines

--- run2.sh
+++ run2.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-unzip data/sat_product.zip -d tmp
+#unzip data/sat_product.zip -d tmp
 
-python ex1.py tmp/*.SAFE
+#python ex1.py tmp/*.SAFE
+
+
+unzip data/sat_product.zip -d /tmp/unzipped
+#debug lines
+ls -l /tmp/unzipped/
+ls -d /tmp/unzipped/*.SAFE
+
+python ex-ndvi4.py $(ls -d /tmp/unzipped/*.SAFE)
